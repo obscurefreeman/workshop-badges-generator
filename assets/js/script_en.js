5114858aec8974c14995f3ddbbf387ce6aaa2ad6
obscurefreeman <1093048022@qq.com>
2023-12-25 17:26:46 +0800

    多语言制作完毕

--- assets/js/script_en.js
+++ assets/js/script_en.js
@@ -4,7 +4,7 @@ sceditor.create(textarea, {
     format: 'bbcode',
     icons: 'monocons',
     style: 'minified/themes/content/obscurefreeman.css',
-    locale: 'cn',
+    // locale: 'cn',
     plugins: 'undo',
 });
 // 主要代码
@@ -22,25 +22,25 @@ function generateURLs(format) {
     id = inputText.replace("https://steamcommunity.com/sharedfiles/filedetails/?id=", "");
     } else {
     // 输入格式不正确，给出错误提示
-    alert("请填写正确的ID或URL！");
+    alert("Please fill in the correct ID or URL!");
     return;
     }
 
     var outputURLs = document.getElementById("outputURLs");
-    var subscriptions = `https://img.shields.io/steam/subscriptions/${id}?style=for-the-badge&label=订阅&color=b4e419`;
-    var downloads = `https://img.shields.io/steam/downloads/${id}?style=for-the-badge&label=下载&color=00adb5`;
-    var views = `https://img.shields.io/steam/views/${id}?style=for-the-badge&label=浏览量&color=ff5719`;
-    var size = `https://img.shields.io/steam/size/${id}?style=for-the-badge&label=模组大小&color=2ea043`;
-    var releasedate = `https://img.shields.io/steam/release-date/${id}?style=for-the-badge&label=发布日期&color=ffb300`;
-    var updatedate = `https://img.shields.io/steam/update-date/${id}?style=for-the-badge&label=更新日期&color=515de9`;
+    var subscriptions = `https://img.shields.io/steam/subscriptions/${id}?style=for-the-badge&color=b4e419`;
+    var downloads = `https://img.shields.io/steam/downloads/${id}?style=for-the-badge&color=00adb5`;
+    var views = `https://img.shields.io/steam/views/${id}?style=for-the-badge&color=ff5719`;
+    var size = `https://img.shields.io/steam/size/${id}?style=for-the-badge&color=2ea043`;
+    var releasedate = `https://img.shields.io/steam/release-date/${id}?style=for-the-badge&color=ffb300`;
+    var updatedate = `https://img.shields.io/steam/update-date/${id}?style=for-the-badge&color=515de9`;
     
     // 生成URLs
-    var url1 = format === "bbcode" ? `[img]${subscriptions}[/img]` : `![订阅](${subscriptions})`;
-    var url2 = format === "bbcode" ? `[img]${downloads}[/img]` : `![下载](${downloads})`;
-    var url3 = format === "bbcode" ? `[img]${views}[/img]` : `![浏览量](${views})`;
-    var url4 = format === "bbcode" ? `[img]${size}[/img]` : `![模组大小](${size})`;
-    var url5 = format === "bbcode" ? `[img]${releasedate}[/img]` : `![发布日期](${releasedate})`;
-    var url6 = format === "bbcode" ? `[img]${updatedate}[/img]` : `![更新日期](${updatedate})`;
+    var url1 = format === "bbcode" ? `[img]${subscriptions}[/img]` : `![Subscriptions](${subscriptions})`;
+    var url2 = format === "bbcode" ? `[img]${downloads}[/img]` : `![Downloads](${downloads})`;
+    var url3 = format === "bbcode" ? `[img]${views}[/img]` : `![Views](${views})`;
+    var url4 = format === "bbcode" ? `[img]${size}[/img]` : `![Size](${size})`;
+    var url5 = format === "bbcode" ? `[img]${releasedate}[/img]` : `![Release Date](${releasedate})`;
+    var url6 = format === "bbcode" ? `[img]${updatedate}[/img]` : `![Update Date](${updatedate})`;
     
     // 显示URLs
     outputURLs.innerHTML = "";
@@ -61,7 +61,7 @@ function generateURLs(format) {
         
         var copyButton = document.createElement("button");
         copyButton.className = "copy-button";
-        copyButton.innerHTML = "复制";
+        copyButton.innerHTML = "Copy";
         copyButton.addEventListener("click", function() {
             copyToClipboard(url);
         });
@@ -81,7 +81,7 @@ function generateURLs(format) {
     var urlContainer6 = createURLContainer(updatedate, url6);
     var copyAllButton = document.createElement("button");
     copyAllButton.className = "copy-all-button";
-    copyAllButton.innerHTML = "复制全部";
+    copyAllButton.innerHTML = "Copy All";
     copyAllButton.addEventListener("click", function() {
         copyAllToClipboard();
     });
@@ -102,7 +102,7 @@ function copyToClipboard(text) {
     tempInput.select();
     document.execCommand("copy");
     document.body.removeChild(tempInput);
-    alert("已复制到剪贴板！");
+    alert("Code copied!");
 }
 
 function copyAllToClipboard() {
@@ -117,5 +117,5 @@ function copyAllToClipboard() {
     tempInput.select();
     document.execCommand("copy");
     document.body.removeChild(tempInput);
-    alert("已复制所有代码到剪贴板！");
+    alert("All code copied!");
 }
