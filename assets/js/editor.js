c68d932a331a3df9628c012bfc01a0026b35cdcb
obscurefreeman <1093048022@qq.com>
2023-12-25 19:16:30 +0800

    Update editor.js

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,16 +1,10 @@
 var textarea = document.getElementById('editor');
-var locale = 'en'; // 默认语言为英语
-
-// 检测网页链接是否包含"/cn"，如果是则将locale设置为中文
-if (window.location.href.includes('/cn')) {
-    locale = 'cn';
-}
 
 sceditor.create(textarea, {
     format: 'bbcode',
     icons: 'monocons',
     style: 'minified/themes/content/obscurefreeman.css',
-    locale: locale, // 根据检测结果设置编辑器的语言
+    locale: 'cn', // 根据检测结果设置编辑器的语言
     plugins: 'undo,plaintext,dragdrop,autoyoutube',
     pastetext: {
         addButton: true,
