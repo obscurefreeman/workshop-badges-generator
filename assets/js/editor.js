19e55cccc50b826520295a9fd7d69e25b9af9078
obscurefreeman <1093048022@qq.com>
2023-12-25 19:12:50 +0800

    尝试不同语言的编辑器

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,23 +1,16 @@
-// 编辑器
-var textarea = document.getElementById('editor_en');
-sceditor.create(textarea, {
-    format: 'bbcode',
-    icons: 'monocons',
-    style: 'minified/themes/content/obscurefreeman.css',
-    // locale: 'cn',
-    plugins: 'undo,plaintext,dragdrop,autoyoutube',
-    pastetext: {
-        addButton: true,
-        enabled: false // Set to true to start in enabled state
-    },
-});
+var textarea = document.getElementById('editor');
+var locale = 'en'; // 默认语言为英语
 
-var textarea = document.getElementById('editor_zh');
+// 检测网页链接是否包含"/cn"，如果是则将locale设置为中文
+if (window.location.href.includes('/cn')) {
+    locale = 'cn';
+}
+
 sceditor.create(textarea, {
     format: 'bbcode',
     icons: 'monocons',
     style: 'minified/themes/content/obscurefreeman.css',
-    locale: 'cn',
+    locale: locale, // 根据检测结果设置编辑器的语言
     plugins: 'undo,plaintext,dragdrop,autoyoutube',
     pastetext: {
         addButton: true,
